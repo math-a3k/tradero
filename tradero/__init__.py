38daa09f9f502bea8e3b0cfd4efb6e566619003d
Rodrigo <matematica.a3k@gmail.com>
2023-09-04 10:32:50 -0300

    Aplastar Bichinho
    - Prevent from failing if Redis is not available (for building in
      ReadTheDocs)

--- tradero/__init__.py
+++ tradero/__init__.py
@@ -3,6 +3,11 @@ from django.core.cache import cache
 
 from .celery import app as celery_app
 
-cache.delete(settings.SYMBOLS_UPDATE_ALL_INDICATORS_KEY)
+try:
+    cache.delete(settings.SYMBOLS_UPDATE_ALL_INDICATORS_KEY)
+except Exception:  # pragma: no cover
+    # Prevent from failing if Redis is not available (for building in ReadTheDocs)
+    pass
+
 
 __all__ = ("celery_app",)
